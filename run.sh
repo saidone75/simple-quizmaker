7d200c2ceb3118fc78be3d34c266b785f511544f
saidone <saidone@saidone.org>
2026-03-28 15:49:53 +0100

    Temporary remove create sample quiz

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 JAVA_OPTS="-Xms64m -Xmx64m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:9000"
-export SPRING_PROFILES_ACTIVE=prod
+export SPRING_PROFILES_ACTIVE=dev
 
-export SUPABASE_DB_PASSWORD=Nq3L2wtpxDxUQHSKkJgQoKQzxZduqAsF
+export SUPABASE_DB_PASSWORD=
 
 mvn spring-boot:run -Dspring-boot.run.jvmArguments="$JAVA_OPTS"
